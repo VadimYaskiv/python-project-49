3a8fc4b4fe87ead61df3f1ecc09bddd48ae1cb44
Vadim <vadimyaskiv89@gmail.com>
2023-03-30 10:56:31 +0300

    Correct #15 mentor's comments_3

--- brain_games/games/even_check.py
+++ brain_games/games/even_check.py
@@ -5,17 +5,16 @@ def game_task():
     return 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def quest_num_f():
-    QUEST_NUM = random.randint(1, 1000)
-    return QUEST_NUM
+def is_even(num):
+    return num % 2 == 0
 
 
 def quest_answ_pair():
-    quest_num = quest_num_f()
+    QUEST_NUM = random.randint(1, 1000)
 
-    if quest_num % 2 == 0:
+    if is_even(QUEST_NUM):
         right_answer = 'yes'
     else:
         right_answer = 'no'
 
-    return quest_num, right_answer
+    return QUEST_NUM, right_answer
--- brain_games/games/prime_check.py
+++ brain_games/games/prime_check.py
@@ -5,21 +5,22 @@ def game_task():
     return 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def quest_num_f():
-    QUEST_NUM = random.randint(1, 100)
-    return QUEST_NUM
+def is_prime(num):
+    i = 2
+    while i * i <= num:
+        if num % i == 0:
+            return False
+        i += 1
+    else:
+        return True
 
 
 def quest_answ_pair():
-    quest_num = quest_num_f()
+    QUEST_NUM = random.randint(1, 100)
 
-    i = 2
-    while i * i <= quest_num:
-        if quest_num % i == 0:
-            right_answer = 'no'
-            break
-        i += 1
-    else:
+    if is_prime(QUEST_NUM):
         right_answer = 'yes'
+    else:
+        right_answer = 'no'
 
-    return quest_num, right_answer
+    return QUEST_NUM, right_answer
