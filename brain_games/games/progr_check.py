5d4444ee93576841e7394686cd3642747fe19deb
Vadim <vadimyaskiv89@gmail.com>
2023-03-30 11:14:58 +0300

    Correct #16 mentor's comments_3

--- brain_games/games/progr_check.py
+++ brain_games/games/progr_check.py
@@ -5,23 +5,17 @@ def game_task():
     return 'What number is missing in the progression?'
 
 
-def members():
+def progress():
     INITIAL_TERM = random.randint(1, 11)
     FINAL_TERM = random.randint(52, 70)
     COMMON_DIFF = random.randint(2, 10)
-    return INITIAL_TERM, FINAL_TERM, COMMON_DIFF
-
-
-def str_progr():
-    initial_term, final_term, common_diff = members()
-    progr = list(range(initial_term, final_term, common_diff))
-    change_num = random.choice(progr)
-    progr[progr.index(change_num)] = '..'
-    quest_num = ' '.join(map(str, progr))
-    return change_num, quest_num
+    progr = list(range(INITIAL_TERM, FINAL_TERM, COMMON_DIFF))
+    return progr
 
 
 def quest_answ_pair():
-    change_num, quest_num = str_progr()
-    right_answer = str(change_num)
-    return quest_num, right_answer
+    progr = progress()
+    change_num = random.choice(progr)
+    progr[progr.index(change_num)] = '..'
+    quest_num = ' '.join(map(str, progr))
+    return quest_num, str(change_num)
