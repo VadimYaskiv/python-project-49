77d9ea70d756e0ef4dbae8f5db93264cc3f0058e
Vadim <vadimyaskiv89@gmail.com>
2023-04-05 15:43:12 +0300

    Correct #16 mentor's comments_4

--- brain_games/games/progr_check.py
+++ brain_games/games/progr_check.py
@@ -6,34 +6,22 @@ def game_task():
 
 
 INIT_TERM_INTERVAL = (1, 11)
-FINAL_TERM_INTERVAL = (52, 70)
+LENGTH_INTERVAL = (52, 70)
 DIFF_INTERVAL = (2, 10)
 
 
-def initial_term():
-    return random.randint(*INIT_TERM_INTERVAL)
-
-
-def final_term():
-    return random.randint(*FINAL_TERM_INTERVAL)
-
-
-def common_diff():
-    return random.randint(*DIFF_INTERVAL)
-
-
 def make_progress(initial_term, final_term, common_diff):
-    initial_term = initial_term()
-    final_term = final_term()
-    common_diff = common_diff()
-    progr = list(range(initial_term, final_term, common_diff))
-    return progr
+    return list(range(initial_term, final_term, common_diff))
 
 
 def quest_answ_pair():
-    progr = make_progress(initial_term, final_term, common_diff)
-    change_index = random.randrange(len(progr))
-    change_num = progr[change_index]
-    progr[change_index] = '..'
-    quest_num = ' '.join(map(str, progr))
+    initial_term = random.randint(*INIT_TERM_INTERVAL)
+    length = random.randint(*LENGTH_INTERVAL)
+    common_diff = random.randint(*DIFF_INTERVAL)
+
+    progression = make_progress(initial_term, length, common_diff)
+    change_index = random.randrange(len(progression))
+    change_num = progression[change_index]
+    progression[change_index] = '..'
+    quest_num = ' '.join(map(str, progression))
     return quest_num, str(change_num)
